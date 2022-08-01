8bec75c5fcc90d66582fbaab3ca737159614ae16
Woo Han <hanwj100@gmail.com>
2022-08-01 08:51:11 -0700

    Create getPlayerChoice function

---
+++ javascript.js
@@ -0,0 +1,32 @@
+/*
+Prompt the user to pick rock, papers or scissors and store the selection.
+
+Generate a random choice for the computer and store its "selection" as well.
+
+Compare the userSelection and computerSelection to determine the winner.
+
+Display the result of the round to the user via console.
+
+Add 1 to the winner's total score.
+
+Replay the game until 5 games total are played.
+
+Compare the userScore and computerScore to determine the winner.
+
+Display the result of the game to the user via console.
+*/
+
+//takes user input and returns formatted input if valid, else re-asks for input. 
+function getPlayerChoice() {
+    let validInput = false;
+    while (!validInput) {
+        let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            validInput = true;
+            return choice;
+        }
+        else{
+            alert("Please make a valid choice.");
+        }
+    }
+}
