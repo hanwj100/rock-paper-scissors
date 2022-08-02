9cff5e661a51b176c6dc9327b0a01f64afc58e5d
Woo Han <hanwj100@gmail.com>
2022-08-02 00:29:47 -0700

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ Compare the userScore and computerScore to determine the winner.
 Display the result of the game to the user via console.
 */
 
-//takes user input and returns formatted input if valid, else re-asks for input. 
+//takes user input and returns formatted input if valid, else re-asks for input 
 function getPlayerChoice() {
     let validInput = false;
     while (!validInput) {
@@ -25,7 +25,7 @@ function getPlayerChoice() {
             validInput = true;
             return choice;
         }
-        else{
+        else {
             alert("Please make a valid choice.");
         }
     }
@@ -38,5 +38,37 @@ function getComputerChoice() {
     return choiceArray[choice];
 }
 
-console.log(getComputerChoice());
+//takes player and computer choice as input and declares the winner of the round
+function playRound(playerSelection, computerSelection) {
+    //same vs same = tie
+    //rock vs scissor = rock
+    //scissor vs paper = scissor
+    //paper vs rock = paper
 
+    //if the two selections are equal to each other
+    //tie
+
+    //if either selection is rock and either selection is scissor
+    //winner is rock
+
+    //if either selection is scissor and either selection is paper
+    //winner is scissor
+
+    //if either selection is paper and either selection is rock
+    //winner is paper
+
+    let winner;
+    if (playerSelection === computerSelection){
+        winner = "tie";
+    }
+    else if ((playerSelection === "rock" || computerSelection === "rock") && (playerSelection === "scissors" || computerSelection === "scissors")) {
+        winner = (playerSelection === "rock") ? "player" : "computer";
+    }
+    else if ((playerSelection === "paper" || computerSelection === "paper") && (playerSelection === "scissors" || computerSelection === "scissors")){
+        winner = (playerSelection === "scissors") ? "player" : "computer";
+    }
+    else if ((playerSelection === "rock" || computerSelection === "rock") && (playerSelection === "paper" || computerSelection === "paper")){
+        winner = (playerSelection === "paper") ? "player" : "computer";
+    }
+    return winner;
+}
