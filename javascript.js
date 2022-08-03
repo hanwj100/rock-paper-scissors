65a6bf23818c23d50736137a93c85c4f0718a29e
Woo Han <hanwj100@gmail.com>
2022-08-03 10:19:34 -0700

    Delete unnecessary comments
    Though the pseudocode before some of the functions were useful while
    I was writing the code, I do not want to include unnecessary comments
    that make the code difficult to read. I think that the simple one line
    explanations before each function are sufficient and the code itself
    is clear enough.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,4 @@
-/*
-Prompt the user to pick rock, papers or scissors and store the selection.
 
-Generate a random choice for the computer and store its "selection" as well.
-
-Compare the userSelection and computerSelection to determine the winner.
-
-Display the result of the round to the user via console.
-
-Add 1 to the winner's total score.
-
-Replay the game until 5 games total are played.
-
-Compare the userScore and computerScore to determine the winner.
-
-Display the result of the game to the user via console.
-*/
 
 //takes user input and returns formatted input if valid, else re-asks for input 
 function getPlayerChoice() {
@@ -47,23 +31,6 @@ function Winner(who, message) {
 
 //takes player and computer choice as input and declares the winner of the round
 function determineWinner(playerSelection, computerSelection) {
-  //same vs same = tie
-  //rock vs scissor = rock
-  //scissor vs paper = scissor
-  //paper vs rock = paper
-
-  //if the two selections are equal to each other
-  //tie
-
-  //if either selection is rock and either selection is scissor
-  //winner is rock
-
-  //if either selection is scissor and either selection is paper
-  //winner is scissor
-
-  //if either selection is paper and either selection is rock
-  //winner is paper
-
   const winner = new Winner();
   if (playerSelection === computerSelection) {
     winner.who = "tie";
@@ -84,14 +51,7 @@ function determineWinner(playerSelection, computerSelection) {
   return winner;
 }
 
-//best out of 5 games, which means that the first to 3 points wins
-//it does not necessarily mean that there will be 5 games played total, there may be more if ties occur
-
-//take the choice of each player and determine a winner
-//print the winner message to the screen
-//add one point to the winner's score
-//play again if tied
-//stop after 5 points have been distributed
+//first to 3 points wins, there may be more than 5 games if ties occur
 function game() {
   let playerScore = 0;
   let computerScore = 0;
